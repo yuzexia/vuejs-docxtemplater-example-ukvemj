6fdc710296c4faa9c3a0e77b258934b8e16e630c
yuzexia <327932801@qq.com>
2023-04-03 11:14:42 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vuejs-docxtemplater-example-ukvemj
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vuejs-docxtemplater-example-ukvemj)
---
+++ app.js
@@ -0,0 +1,12 @@
+import Button from './button.component';
+
+export const App = {
+  components: { Button },
+  template: `
+    <div>
+
+      <h2>Button Component</h2>
+  	  <Button />
+    </div>
+  `
+}
